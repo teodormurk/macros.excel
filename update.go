e5322831781e7051e3ea001c713d91005c0e232a
Teodor Murk <teodormurk@MacBook-Air-Teodor.local>
2026-06-01 14:16:01 +0300

    Write VBScript in Windows-1251, use short path for workbook

--- update.go
+++ update.go
@@ -66,7 +66,10 @@ func updateWin(absXlsm string) {
 	vbs := fmt.Sprintf(`Set excel = CreateObject("Excel.Application")
 excel.Visible = True
 excel.DisplayAlerts = False
-Set wb = excel.Workbooks.Open("%s")
+Set fso = CreateObject("Scripting.FileSystemObject")
+Set f = fso.GetFile("%s")
+shortPath = f.ShortPath
+Set wb = excel.Workbooks.Open(shortPath)
 On Error Resume Next
 excel.Run "InstallModules"
 If Err.Number <> 0 Then
@@ -85,7 +88,7 @@ wb.Close
 excel.Quit`, absXlsm)
 
 	tmp := filepath.Join(os.TempDir(), "update_macros.vbs")
-	os.WriteFile(tmp, []byte(vbs), 0644)
+	os.WriteFile(tmp, utf8ToWin1251([]byte(vbs)), 0644)
 	defer os.Remove(tmp)
 
 	out, err := exec.Command("cscript", "//nologo", tmp).CombinedOutput()
@@ -94,6 +97,69 @@ excel.Quit`, absXlsm)
 		fatal("cscript error: %s", err)
 	}
 	fmt.Println("Done!")
+}
+
+func utf8ToWin1251(data []byte) []byte {
+	result := make([]byte, 0, len(data))
+	for i := 0; i < len(data); {
+		b := data[i]
+		switch {
+		case b < 0x80:
+			result = append(result, b)
+			i++
+		case b == 0xD0:
+			if i+1 < len(data) {
+				b2 := data[i+1]
+				i += 2
+				switch {
+				case b2 == 0x81:
+					result = append(result, 0xA1)
+				case b2 >= 0x90 && b2 <= 0xBF:
+					result = append(result, b2+0x30)
+				default:
+					result = append(result, '?')
+				}
+			} else {
+				i++
+			}
+		case b == 0xD1:
+			if i+1 < len(data) {
+				b2 := data[i+1]
+				i += 2
+				switch {
+				case b2 == 0x91:
+					result = append(result, 0xB8)
+				case b2 >= 0x80 && b2 <= 0x8F:
+					result = append(result, b2+0x70)
+				default:
+					result = append(result, '?')
+				}
+			} else {
+				i++
+			}
+		case b == 0xC2:
+			if i+1 < len(data) {
+				b2 := data[i+1]
+				i += 2
+				switch b2 {
+				case 0x85:
+					result = append(result, 0xA5)
+				case 0x89:
+					result = append(result, 0xA9)
+				case 0x8A:
+					result = append(result, 0xAA)
+				default:
+					result = append(result, '?')
+				}
+			} else {
+				i++
+			}
+		default:
+			result = append(result, '?')
+			i++
+		}
+	}
+	return result
 }
 
 func fatal(format string, args ...any) {
